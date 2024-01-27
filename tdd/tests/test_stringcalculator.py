ad4390fb08529efb530eb20623290a9764478419
Bdeeux <pm51muq@eduvaud.ch>
2024-01-27 19:10:40 +0100

    R : multiplication de nombres en excluant ceux supérieurs à 1000

--- tdd/tests/test_stringcalculator.py
+++ tdd/tests/test_stringcalculator.py
@@ -21,24 +21,6 @@ def test_Add_ParamSeul_ReturnMeme():
     # assert
     assert somme == mon_resultat
 
-def test_Add_ParamAlphabet_ReturnZero():
-    # arrange
-    alphabet = "a;b;c;d;e;f;g;h;i;j;k;l;m;n;o;p;q;r;s;t;u;v;w;x;y;z"
-    mon_resultat = 0
-    # act
-    somme = StringCalculator.Add(alphabet)
-    # assert
-    assert somme == mon_resultat
-
-def test_Add_ParamNegativeNumber_ReturnNegativeNumber():
-    # arrange
-    mon_param = "5;-10;15"
-    mon_resultat = 10  # La somme attendue est de 10 (5 + (-10) + 15)
-    # act
-    somme = StringCalculator.Add(mon_param)
-    # assert
-    assert somme == mon_resultat
-
 def test_Multiply():
     # arrange
     mon_param = "3;4;1;1001"
