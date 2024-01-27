b0f0778fd320a310b1ea8ebb8b5b8057f4a4f524
Bdeeux <pm51muq@eduvaud.ch>
2024-01-27 18:44:32 +0100

    R : multiplication de nombres en excluant ceux supérieurs à 1000

--- tdd/src/stringcalculator.py
+++ tdd/src/stringcalculator.py
@@ -1,8 +1,20 @@
-class StringCalculator :
+class StringCalculator:
+
+    @staticmethod
     def Add(numbers):
+        parts = numbers.split(';')
+        sum = 0
+        for part in parts:
+            try:
+                number = int(part)
+            except ValueError:
+                number = 0
+            if number <= 1000:
+                sum += number
+        return sum
+    
+    # Rouge
+    def Multiply(numbers):
+        # Implémentation temporaire
         return 0
-        #if numbers == "":
-            #return 0
-        #else :
-            #return 5
 
--- tdd/tests/test_stringcalculator.py
+++ tdd/tests/test_stringcalculator.py
@@ -1,5 +1,6 @@
 from tdd.src.stringcalculator import StringCalculator
 
+
 #Test que un paramètre vide retourne zéro : ""==0
 def test_Add_ParamVide_ReturnZero():
     # arrange
@@ -19,3 +20,12 @@ def test_Add_ParamSeul_ReturnMeme():
     somme = StringCalculator.Add(mon_param)
     # assert
     assert somme == mon_resultat
+
+def test_Multiply():
+    # arrange
+    mon_param = "3;4;1;1001"
+    mon_resultat = 12  # La multiplication attendue est de 3 * 4 * 1 = 12
+    # act
+    produit = StringCalculator.Multiply(mon_param)
+    # assert
+    assert produit == mon_resultat
