8070940e7361d960607ca7b1c4657f537b84f974
Bdeeux <pm51muq@eduvaud.ch>
2024-01-28 14:53:45 +0100

    G : Modification de la fonction Multiply pour qu'elle puisse multiplier des nombres négatifs jusqu'à -1000

--- tdd/src/stringcalculator.py
+++ tdd/src/stringcalculator.py
@@ -56,7 +56,7 @@ class StringCalculator:
 # Deuxième itération # 
 
 #Red
-    def Multiply(numbers):
+    #def Multiply(numbers):
         def parse_and_filter_number(num_str):
             try:
                 number = int(num_str)
@@ -72,7 +72,7 @@ class StringCalculator:
         return product
     
 #Green
-    #def Multiply(numbers):
+    def Multiply(numbers):
         def parse_and_filter_number(num_str):
             try:
                 number = int(num_str)
